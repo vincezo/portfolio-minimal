4616a8129b63d83d1c8d02f9c1116e866fac75d9
vincezo <vinceczuno@gmail.com>
2021-04-21 17:49:46 +0800

    Updated cypress test

--- cypress/e2e/main.spec.js
+++ cypress/e2e/main.spec.js
@@ -9,7 +9,7 @@ describe("IndexPage", () => {
 
   it("renders all sections", () => {
     cy.get("#hero").should("exist")
-    cy.get("#articles").should("exist")
+    // cy.get("#articles").should("exist")
     cy.get("#about").should("exist")
     cy.get("#skills").should("exist")
     cy.get("#expertise").should("exist")
@@ -21,33 +21,33 @@ describe("IndexPage", () => {
     cy.findByTestId("animated-heading").should("have.css", "opacity", "1")
   })
 
-  it("references legal pages", () => {
-    cy.findByTestId("footer-links")
-      .findByText(/imprint/i)
-      .should("have.attr", "href")
-      .and("include", "imprint")
-    cy.findByTestId("footer-links")
-      .findByText(/privacy/i)
-      .should("have.attr", "href")
-      .and("include", "privacy")
-  })
+  // it("references legal pages", () => {
+  //   cy.findByTestId("footer-links")
+  //     .findByText(/imprint/i)
+  //     .should("have.attr", "href")
+  //     .and("include", "imprint")
+  //   cy.findByTestId("footer-links")
+  //     .findByText(/privacy/i)
+  //     .should("have.attr", "href")
+  //     .and("include", "privacy")
+  // })
 
-  it("renders other pages", () => {
-    cy.get("#layout-wrapper").then(elem => {
-      if (elem.attr("data-useCookieBar") === "true") {
-        cy.get("button#confirm").click()
-        cy.findByTestId("footer-links")
-          .findByText(/imprint/i)
-          .click()
-        cy.findByTestId("heading").should("exist")
-      } else {
-        cy.findByTestId("footer-links")
-          .findByText(/imprint/i)
-          .click()
-        cy.findByTestId("heading").should("exist")
-      }
-    })
-  })
+  // it("renders other pages", () => {
+  //   cy.get("#layout-wrapper").then(elem => {
+  //     if (elem.attr("data-useCookieBar") === "true") {
+  //       cy.get("button#confirm").click()
+  //       cy.findByTestId("footer-links")
+  //         .findByText(/imprint/i)
+  //         .click()
+  //       cy.findByTestId("heading").should("exist")
+  //     } else {
+  //       cy.findByTestId("footer-links")
+  //         .findByText(/imprint/i)
+  //         .click()
+  //       cy.findByTestId("heading").should("exist")
+  //     }
+  //   })
+  // })
 
   it("renders cookie bar if activated", () => {
     cy.get("#layout-wrapper").then(elem => {
